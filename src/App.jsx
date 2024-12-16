9001f0da0ec82a53a1c009a67faf6ddeecbb9202
junaid <junaid.sheikh.1800@gmail.com>
2024-12-16 13:26:57 +0530

    initial files added

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Myappoinments from './pages/Myappoinments'
+import Myprofile from "./pages/Myprofile"
+import Contact from './pages/Contact'
+import Appoinment from './pages/Appoinment'
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <div>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/myappoinments' element={<Myappoinments />}/>
+        <Route path='/myprofile' element={<Myprofile />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/appoinment' element={<Appoinment />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
