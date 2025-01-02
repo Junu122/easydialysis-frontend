6dbe801aa5e97af6e14ff23197c77f5ed45875c6
junaid <junaid.sheikh.1800@gmail.com>
2025-01-02 16:23:38 +0530

    what we do componet added to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Homebanner from '../components/Homebanner'
 import Servicecontainer from '../components/Servicecontainer'
+import Whatwedo from '../components/Whatwedo'
 const Home = () => {
   return (
     <div>
       <Homebanner />
       <Servicecontainer />
+      <Whatwedo />
     </div>
   )
 }
