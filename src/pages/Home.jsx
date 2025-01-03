52dcf9aa37149e076383b53243e30420cfc3f45b
junaid <junaid.sheikh.1800@gmail.com>
2025-01-03 17:53:38 +0530

    minor changes on home page and index.html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Homebanner from '../components/Homebanner'
 import Servicecontainer from '../components/Servicecontainer'
 import Whatwedo from '../components/Whatwedo'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <div>
       <Homebanner />
       <Servicecontainer />
       <Whatwedo />
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
